1602b5f805208a998acee25682ba1fe6944d719e
Pedro <67802288+pedromachakio@users.noreply.github.com>
2022-01-27 17:41:54 -0100

    Initial project with required dependencies

---
+++ src/main/java/pedromachakio/com/github/restapiblogapp/RestApiBlogAppApplication.java
@@ -0,0 +1,13 @@
+package pedromachakio.com.github.restapiblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedromachakio/com/github/restapiblogapp/RestApiBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pedromachakio.com.github.restapiblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
